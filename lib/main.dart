add0eaad11b65bf3267e201238c6dea80fcf4a39
Vishal Borana <vishalborana@Vishals-MacBook-Air.local>
2022-04-28 16:06:18 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// entry point
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // contructor
+  const MyApp({Key? key}) : super(key: key);
+
+// build for UI development
+  @override
+  Widget build(BuildContext context) {
+    // The main build method has to return either a
+    // Material App, Cupertino App or Widgets App
+    return MaterialApp(
+      home: Material(
+          child: Center(
+              child: Container(child: Text('Welcome to 30 days of flutter!')))),
+    );
+  }
+}
