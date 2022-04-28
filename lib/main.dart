46cd171499838c632c8e60412927caff7f7ba251
Vishal Borana <vishalborana@Vishals-MacBook-Air.local>
2022-04-28 18:48:40 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './home_page.dart';
+import 'pages/home_page.dart';
+import 'pages/login_page.dart';
+
 // entry point
 
 void main() {
@@ -15,6 +17,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // The main build method has to return either a
     // Material App, Cupertino App or Widgets App
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
+    );
   }
 }
