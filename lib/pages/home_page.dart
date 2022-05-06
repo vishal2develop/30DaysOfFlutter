85cc64766400a060109f74997324faa5da48f8d8
Vishal Borana <vishalborana@Vishals-MacBook-Air.local>
2022-05-06 19:33:20 +0530

    day 29 completed - http get call

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,16 +2,16 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_catalog/core/store.dart';
+import 'package:flutter_catalog/models/cart.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import 'package:flutter_catalog/models/catalog.dart';
-import 'package:flutter_catalog/widgets/themes.dart';
 
 import '../widgets/home_widgets/catalog_header.dart';
 import '../widgets/home_widgets/catalog_list.dart';
-import '../widgets/home_widgets/catalog_item.dart';
-import '../widgets/home_widgets/catalog_image.dart';
 
 import '../utils/routes.dart';
 
@@ -23,9 +23,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final days = 30;
+  final int days = 30;
 
-  final name = "Hello";
+  final String name = "Hello";
+
+  final url = "https://api.jsonbin.io/b/6275284525069545a32e51eb";
+  Map<String, String> reqHeaders = {
+    'X-Master-Key':
+        "\$2b\$10\$Ea9GJbtst7aCWFUMOBdwO.lFR9cEWGWdqKi7sdU9Kpna1DFCOL/de"
+  };
 
   @override
   void initState() {
@@ -36,8 +42,16 @@ class _HomePageState extends State<HomePage> {
 
   loadData() async {
     await Future.delayed(Duration(seconds: 2));
-    final catalogJson =
-        await rootBundle.loadString("assets/files/catalog.json");
+
+    // getting data from assets
+    // final catalogJson =
+    //     await rootBundle.loadString("assets/files/catalog.json");
+
+    // Getting data from network
+
+    final response = await http.get(Uri.parse(url), headers: reqHeaders);
+    final catalogJson = response.body;
+
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData['products'];
     CatalogModel.items = List.from(productsData)
@@ -49,20 +63,30 @@ class _HomePageState extends State<HomePage> {
   // double pi = 3.14;
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     // replicating array items by passing in a number and referece list item
     // final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
         backgroundColor: context.canvasColor,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, MyRoutes.cartRoute);
-          },
-          child: Icon(
-            CupertinoIcons.cart,
-            color: Colors.white,
-          ),
-          backgroundColor: context.theme.buttonColor,
-        ),
+        floatingActionButton: VxBuilder(
+            builder: (context, _, __) {
+              return FloatingActionButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, MyRoutes.cartRoute);
+                },
+                child: Icon(
+                  CupertinoIcons.cart,
+                  color: Colors.white,
+                ),
+                backgroundColor: context.theme.buttonColor,
+              ).badge(
+                  color: Vx.red500,
+                  size: 22,
+                  count: _cart.items.length,
+                  textStyle: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold));
+            },
+            mutations: {AddMutation, RemoveMutation}),
         body: SafeArea(
             child: Container(
                 padding: Vx.m32,
